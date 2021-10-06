4c71038d4c143bffffcf28843a7486c523f95ea5
ChickenPlays14 <chloe@chickenplays.co.uk>
2021-10-06 23:54:47 +0100

    Expanded upon the WIP 'who are you' response line.

--- gadget.py
+++ gadget.py
@@ -1,7 +1,7 @@
-#Section 1 - Do you know gadget?
+# Section 1 - Do you know gadget?
 know_gadget = input("Hello! Do you know gadget? ")
 
-#Handles if the answer to section 1 is yes
+# Handles if the answer to section 1 is yes
 if know_gadget == "yes":
     answer1 = input("He's so stupid isn't he! ")
     if answer1 == "yes":
@@ -11,11 +11,11 @@ if know_gadget == "yes":
     else:
         print("I do not understand you, you complete imbecile")
 
-#Handles if the answer to section 1 is no
+# Handles if the answer to section 1 is no
 elif know_gadget == "no":
     print("That's a good thing tbh")
 
-#Handles if the answer to section 1 is maybe
+# Handles if the answer to section 1 is maybe
 elif know_gadget == "maybe":
     answer2 = input("What the hell do you mean by maybe? ")
     if answer2 == "idk":
@@ -23,7 +23,7 @@ elif know_gadget == "maybe":
         answer3 = int(input("What is your IQ? "))
         if answer3 == 0:
             print("You should really get yourself checked out if you are really that dense!")
-        elif answer3 <=50:
+        elif answer3 <= 50:
             print("Damn you are pretty stupid, you should probably stop talking to me!")
         elif answer3 <= 100:
             print("Guess you aren't as dumb as I initially thought... you can still get outta here though!")
@@ -35,18 +35,33 @@ elif know_gadget == "maybe":
             else:
                 print("WRONG! You must have lied about your IQ!")
 
-#Handles if you ask the program if you asked
+# Handles if you ask the program if you asked
 elif know_gadget == "did i ask?":
     print("Sorry solid! Didn't know you were that hard bruv.")
 
-#Handles if you ask the program who it is (WIP)
+# Handles if you ask the program who it is (WIP)
 elif know_gadget == "wip_who are you":
     answer4 = input("Why does it matter who I am? ")
+    if answer4 == "clout check":
+        print("Stop talking to me right now, goodbye!")  # END
+    elif answer4 == "it just does":
+        answer4_1 = input("Can you please elaborate! You are wasting my time!! ")
+        if answer4_1 == "no":
+            print("Then bugger off! I'm done talking to you!")  # END
+        elif answer4_1 == "okay":
+            print("You say that but did not elaborate. I'm done talking to you.")  # END
+    elif answer4 == "stranger danger":
+        answer4_2 = input("Fine! I am a python program designed to insult. ")
+        if answer4_2 == "still stranger danger":
+            answer4_3 = input("I LITERALLY JUST TOLD YOU I AM A PYTHON PROGRAM!!! ")
+            if answer4_3 == "dont care":
+                print("Well I don't care about you then!")
+                exit(69)
 
-#The code explains itself
+# The code explains itself
 elif know_gadget == "daddy":
     print("uwu")
 
-#Handles if the user inputs anything but 'yes', 'no' or 'maybe' into section 1
+# Handles if the user inputs anything but 'yes', 'no' or 'maybe' into section 1
 else:
     print("I do not understand you, you complete imbecile")
